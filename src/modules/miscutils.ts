10d93ed0e8d6aaa4ef535e91afff65ab0e4a4193
AspectQuote <joshuamdeeds@gmail.com>
2024-09-08 16:26:26 -0600

    Create custom recursive directory reader because linux :D

---
+++ src/modules/miscutils.ts
@@ -0,0 +1,23 @@
+import fs from "fs-extra";
+import path from "path";
+
+function readDirectoryRecursively(directory: fs.PathLike): string[] {
+    let foundFiles: string[] = [];
+    function readDirectory(directory: fs.PathLike) {
+        const files = fs.readdirSync(directory);
+        files.forEach(file => {
+            const item = path.resolve(`${directory}/${file}`);
+            if (fs.statSync(item).isDirectory()) {
+                readDirectory(item);
+            } else {
+                foundFiles.push(item);
+            }
+        })
+    };
+    readDirectory(directory);
+    return foundFiles;
+}
+
+export {
+    readDirectoryRecursively
+}
--- src/routes/directorystructure.ts
+++ src/routes/directorystructure.ts
@@ -1,8 +1,5 @@
 import * as CCOIcons from './../typedefs';
-import * as config from './../modules/schematics/config';
-import * as path from 'node:path';
-import * as Jimp from 'jimp';
-import * as fs from 'fs-extra';
+import { readDirectoryRecursively } from './../modules/miscutils'
 
 const route: CCOIcons.documentedRoute = {
     routes: ['/directorystructure/'],
@@ -18,9 +15,7 @@ const route: CCOIcons.documentedRoute = {
     },
     responseFunction: async (req, res) => {
         const structure = {
-            filePaths: fs.readdirSync(path.resolve('./../ccicons'), {recursive: true}).map(item => {
-                return path.resolve(`./../ccicons/${item}`);
-            })
+            filePaths: readDirectoryRecursively('./../ccicons')
         };
         res.json(structure);
         return;
