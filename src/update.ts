b830fe17d9c5992014a7e2cbd05235998780a95e
AspectQuote <joshuamdeeds@gmail.com>
2024-09-08 16:02:54 -0600

    Update update.ts

--- src/update.ts
+++ src/update.ts
@@ -2,25 +2,25 @@ import path from "path";
 import { cubeID, prefixID } from "./typedefs";
 import fs from "fs-extra";
 
-const updatedCubes: cubeID[] = [];
+const updatedCubes: cubeID[] = ["illuminati"];
 const updatedPrefixes: prefixID[] = [];
 
 const iconDirectory = `./../ccicons/`
-const allFiles = fs.readdirSync(iconDirectory, { recursive: true }).filter(item => typeof item === "string");
+const allFiles = fs.readdirSync(iconDirectory, { recursive: true }).map(item => item.toString());
 const fileMapFunc = (item: string) => {
     return path.resolve(`${iconDirectory}${item}`);
 };
 
-console.log(allFiles)
-
 const updatedCubeFiles = [...allFiles].filter(file => updatedCubes.find(cubeID => file.includes(`${cubeID}.png`) || file.includes(`${cubeID}.gif`) ) !== undefined).map(fileMapFunc);
+
+console.log(updatedCubeFiles)
 
 const updatedPrefixFiles = [...allFiles].filter(file => updatedPrefixes.find(prefixID => !file.includes('.') && !file.includes('\\') && file.includes(`prefix`) && file.includes(prefixID.toLowerCase())) !== undefined).map(fileMapFunc);
 
 const updatedPrefixCacheDirs = [...allFiles].filter(file => file.startsWith('prefixcache') && !file.endsWith('.png') && !!updatedPrefixes.find(prefixID => file.includes(prefixID.toLowerCase()))).map(fileMapFunc)
 
 updatedCubeFiles.forEach(filePath => {
-    fs.unlinkSync(filePath);
+    // fs.unlinkSync(filePath);
     console.log(`Deleted Image: ${filePath}`);
 });
 
