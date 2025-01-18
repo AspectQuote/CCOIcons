5eb8cb98f178dc039e0f6ff9ace6a990048c20ad
AspectQuote <joshuamdeeds@gmail.com>
2025-01-18 02:07:45 -0700

    Update update.ts

--- src/update.ts
+++ src/update.ts
@@ -9,49 +9,51 @@ import { boxID } from "./modules/schematics/boxes";
     const updatedPrefixes: prefixID[] = [];
     const updatedBoxes: boxID[] = [];
 
-    const iconDirectory = `./../ccicons/`
-    const allFiles: string[] = await readDirectoryRecursively(iconDirectory);
-    
-    const updatedCubeFiles = [...allFiles].filter(file => updatedCubes.find(cubeID => file.includes(`${cubeID}.png`) || file.includes(`${cubeID}.gif`) ) !== undefined)
-    
-    const updatedPrefixFiles = [...allFiles].filter(file => updatedPrefixes.find(prefixID => !file.includes('.') && file.includes(`prefix`) && file.includes(prefixID.toLowerCase())) !== undefined)
-    
-    const updatedPrefixCacheDirs = [...allFiles].filter(file => file.includes('prefixcache') && !file.endsWith('.png') && !!updatedPrefixes.find(prefixID => file.includes(prefixID.toLowerCase())))
-    
-    const updatedBoxFiles = [...allFiles].filter(file => file.includes('boxicons') && !!updatedBoxes.find(boxID => file.includes(boxID.toLowerCase())))
-    
-    updatedCubeFiles.forEach(filePath => {
-        if (fs.existsSync(filePath)) {
-            fs.unlinkSync(filePath);
-            console.log(`Deleted Image: ${filePath}`);
-        }
-    });
-    
-    updatedPrefixFiles.forEach(filePath => {
-        if (fs.existsSync(filePath)) {
-            fs.rmSync(filePath, { recursive: true, force: true });
-            console.log(`Deleted Prefix Directory: ${filePath}`);
-        }
-    });
-    
-    updatedPrefixCacheDirs.forEach(filePath => {
-        if (fs.existsSync(filePath)) {
-            fs.rmSync(filePath, { recursive: true, force: true });
-            console.log(`Deleted Prefix Cache Directory: ${filePath}`);
-        }
-    });
-    
-    updatedBoxFiles.forEach(filePath => {
-        if (fs.existsSync(filePath)) {
-            fs.rmSync(filePath, { recursive: true, force: true });
-            console.log(`Deleted Box Image: ${filePath}`);
-        }
-    });
-    
-    console.log(`Update Complete.
-    Removed ${updatedCubeFiles.length} Cube Images.
-    Removed ${updatedPrefixFiles.length} Prefix Directories.
-    Removed ${updatedPrefixCacheDirs.length} Prefix Cache Directories.
-    Removed ${updatedBoxFiles.length} Box Images.
-    `)
+    if ((updatedCubes.length + updatedPrefixes.length + updatedBoxes.length) > 0) {
+        const iconDirectory = `./../ccicons/`
+        const allFiles: string[] = await readDirectoryRecursively(iconDirectory);
+        
+        const updatedCubeFiles = [...allFiles].filter(file => updatedCubes.find(cubeID => file.includes(`${cubeID}.png`) || file.includes(`${cubeID}.gif`) ) !== undefined)
+        
+        const updatedPrefixFiles = [...allFiles].filter(file => updatedPrefixes.find(prefixID => !file.includes('.') && file.includes(`prefix`) && file.includes(prefixID.toLowerCase())) !== undefined)
+        
+        const updatedPrefixCacheDirs = [...allFiles].filter(file => file.includes('prefixcache') && !file.endsWith('.png') && !!updatedPrefixes.find(prefixID => file.includes(prefixID.toLowerCase())))
+        
+        const updatedBoxFiles = [...allFiles].filter(file => file.includes('boxicons') && !!updatedBoxes.find(boxID => file.includes(boxID.toLowerCase())))
+        
+        updatedCubeFiles.forEach(filePath => {
+            if (fs.existsSync(filePath)) {
+                fs.unlinkSync(filePath);
+                console.log(`Deleted Image: ${filePath}`);
+            }
+        });
+        
+        updatedPrefixFiles.forEach(filePath => {
+            if (fs.existsSync(filePath)) {
+                fs.rmSync(filePath, { recursive: true, force: true });
+                console.log(`Deleted Prefix Directory: ${filePath}`);
+            }
+        });
+        
+        updatedPrefixCacheDirs.forEach(filePath => {
+            if (fs.existsSync(filePath)) {
+                fs.rmSync(filePath, { recursive: true, force: true });
+                console.log(`Deleted Prefix Cache Directory: ${filePath}`);
+            }
+        });
+        
+        updatedBoxFiles.forEach(filePath => {
+            if (fs.existsSync(filePath)) {
+                fs.rmSync(filePath, { recursive: true, force: true });
+                console.log(`Deleted Box Image: ${filePath}`);
+            }
+        });
+        
+        console.log(`Update Complete.
+        Removed ${updatedCubeFiles.length} Cube Images.
+        Removed ${updatedPrefixFiles.length} Prefix Directories.
+        Removed ${updatedPrefixCacheDirs.length} Prefix Cache Directories.
+        Removed ${updatedBoxFiles.length} Box Images.
+        `)
+    }
 })()
